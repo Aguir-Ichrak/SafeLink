dd772265c76457861175ada320d3c0641bafc179
Ichrak Aguir <ichrakaguir123@gmail.com>
2024-03-30 02:42:48 -1100

    edit Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LiaUserSlashSolid } from "react-icons/lia";
 import { PiUserSwitchThin } from "react-icons/pi";
 import WelcomeBanner from "../partials/dashboard/WelcomeBanner";
@@ -10,14 +10,42 @@ import DashboardCard08 from "../partials/dashboard/DashboardCard08";
 import PostModal from "./posts/PostModal";
 import DashboardCard04 from "../partials/dashboard/DashboardCard04";
 import DashboardCard06 from "../partials/dashboard/DashboardCard06";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store/UserReducer";
+import { fetchBuildings } from "../store/BuildingReducer";
 
 function Dashboard() {
   const curentUser = useSelector((state) => {
     return state.users.curentUser;
   });
+  const dispatch = useDispatch();
 
-  // const [sidebarOpen, setSidebarOpen] = useState(false);
+  const buildings = useSelector((state) => state.buildings.buildings);
+
+  const users = useSelector((state) => {
+    return state.users.users;
+  });
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+    dispatch(fetchBuildings());
+  }, [dispatch]);
+
+  const activeUsers = () => {
+    return users.filter((user) => user.active == true).length;
+  };
+
+  const inactiveUsers = () => {
+    return users.filter((user) => user.active == false).length;
+  };
+
+  const AvailableBuildings = () => {
+    return buildings.filter((building) => building.status == false).length;
+  };
+
+  const ReservedBuildings = () => {
+    return buildings.filter((building) => building.status == true).length;
+  };
 
   return (
     <main>
@@ -39,76 +67,69 @@ function Dashboard() {
         ) : null}
 
         {curentUser && curentUser.role == "user" ? (
-         
-            <div className="grid grid-cols-4 gap-4">
-              <div className="bg-white rounded-sm border border-slate-200	col-span-1 sm:col-span-1 mb-6	 p-5 items-center shadow-lg dark:border-slate-700 dark:bg-slate-800 flex justify-between">
-                <div>
-                  <h5 class="text-2xl font-semibold text-gray-600 dark:text-white">
-                    82
-                  </h5>
-                  <span className="text-sm	text-slate-400 uppercase">
-                    active users
-                  </span>
-                </div>
-                <div className="bg-transparent">
-                  <PiUserSwitchThin className="text-indigo-200 text-7xl dark:text-indigo-500	" />
-                </div>
-              </div>
-              <div className="bg-white rounded-sm border border-slate-200	col-span-1 sm:col-span-1 mb-6	 p-5 items-center shadow-lg dark:border-slate-700 dark:bg-slate-800 flex justify-between">
-                <div>
+          <div className="grid grid-cols-4 gap-4">
+            <div className="bg-white rounded-sm border border-slate-200	col-span-1 sm:col-span-1 mb-6	 p-5 items-center shadow-lg dark:border-slate-700 dark:bg-slate-800 flex justify-between">
+              <div>
                 <h5 class="text-2xl font-semibold text-gray-600 dark:text-white">
-                    82
-                  </h5>
-                  <span className="text-sm	text-slate-400 uppercase">
-                    inactive users
-                  </span>
-                </div>
-                <div className="bg-transparent">
-                  <LiaUserSlashSolid className="text-indigo-200 text-6xl dark:text-indigo-500	" />
-                </div>
+                  {activeUsers()}
+                </h5>
+                <span className="text-sm	text-slate-400 uppercase">
+                  active users
+                </span>
               </div>
-              <div className="bg-white rounded-sm border border-slate-200	col-span-1 sm:col-span-1 mb-6	 p-5 items-center shadow-lg dark:border-slate-700 dark:bg-slate-800 flex justify-between">
-                <div>
-                <h5 class="text-2xl font-semibold text-gray-600 dark:text-white">
-                    82
-                  </h5>
-                  <span className="text-sm	text-slate-400 uppercase">
-                    available buildings
-                  </span>
-                </div>
-                <div className="bg-transparent">
-                  <BsBuildingSlash className="text-indigo-200 text-5xl dark:text-indigo-500	" />
-                </div>
-              </div>
-              <div className="bg-white rounded-sm border border-slate-200	col-span-1 sm:col-span-1 mb-6	 p-5 items-center shadow-lg dark:border-slate-700 dark:bg-slate-800 flex justify-between">
-                <div>
-                <h5 class="text-2xl font-semibold text-gray-600 dark:text-white">
-                    82
-                  </h5>
-                  <span className="text-sm	text-slate-400 uppercase">
-                    reserved buildings
-                  </span>
-                </div>
-                <div className="bg-transparent">
-                  <BsBuildingLock className="text-indigo-200 text-5xl dark:text-indigo-500	" />
-                </div>
+              <div className="bg-transparent">
+                <PiUserSwitchThin className="text-indigo-200 text-7xl dark:text-indigo-500	" />
               </div>
             </div>
-          
+            <div className="bg-white rounded-sm border border-slate-200	col-span-1 sm:col-span-1 mb-6	 p-5 items-center shadow-lg dark:border-slate-700 dark:bg-slate-800 flex justify-between">
+              <div>
+                <h5 class="text-2xl font-semibold text-gray-600 dark:text-white">
+                  {inactiveUsers()}
+                </h5>
+                <span className="text-sm	text-slate-400 uppercase">
+                  inactive users
+                </span>
+              </div>
+              <div className="bg-transparent">
+                <LiaUserSlashSolid className="text-indigo-200 text-6xl dark:text-indigo-500	" />
+              </div>
+            </div>
+            <div className="bg-white rounded-sm border border-slate-200	col-span-1 sm:col-span-1 mb-6	 p-5 items-center shadow-lg dark:border-slate-700 dark:bg-slate-800 flex justify-between">
+              <div>
+                <h5 class="text-2xl font-semibold text-gray-600 dark:text-white">
+                  {AvailableBuildings()}
+                </h5>
+                <span className="text-sm	text-slate-400 uppercase">
+                  available buildings
+                </span>
+              </div>
+              <div className="bg-transparent">
+                <BsBuildingSlash className="text-indigo-200 text-5xl dark:text-indigo-500	" />
+              </div>
+            </div>
+            <div className="bg-white rounded-sm border border-slate-200	col-span-1 sm:col-span-1 mb-6	 p-5 items-center shadow-lg dark:border-slate-700 dark:bg-slate-800 flex justify-between">
+              <div>
+                <h5 class="text-2xl font-semibold text-gray-600 dark:text-white">
+                  {ReservedBuildings()}
+                </h5>
+                <span className="text-sm	text-slate-400 uppercase">
+                  reserved buildings
+                </span>
+              </div>
+              <div className="bg-transparent">
+                <BsBuildingLock className="text-indigo-200 text-5xl dark:text-indigo-500	" />
+              </div>
+            </div>
+          </div>
         ) : null}
-         {curentUser && curentUser.role == "user" ? (
-         
-       
-         <div className="grid grid-cols-12 gap-6 w-full">
-           <DashboardCard04 />
-           <DashboardCard06 />
-         </div>
-     ) : null}
+        {curentUser && curentUser.role == "user" ? (
+          <div className="grid grid-cols-12 gap-6 w-full">
+            <DashboardCard04 />
+            <DashboardCard06 AB={AvailableBuildings()} RB={ReservedBuildings()} />
+          </div>
+        ) : null}
       </div>
     </main>
-
-    // </div>
-    // </div>
   );
 }
 
--- src/partials/dashboard/DashboardCard06.jsx
+++ src/partials/dashboard/DashboardCard06.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DoughnutChart from '../../charts/DoughnutChart';
 import { FaBuildingShield } from 'react-icons/fa6';
 // Import utilities
 import { tailwindConfig } from '../../utils/Utils';
 
-function DashboardCard06() {
+function DashboardCard06(data) {
+  console.log(data.AB)
 
   const chartData = {
-    labels: ['Block 01', 'Block 02', 'Block 03'],
+    labels: ['Reserved', 'Available'],
     datasets: [
       {
         label: 'Buildings',
         data: [
-          35, 30, 35,
+          data.AB, data.RB
         ],
         backgroundColor: [
           tailwindConfig().theme.colors.indigo[500],
@@ -29,6 +30,10 @@ function DashboardCard06() {
     ],
   };
 
+  useEffect(() => {chartData.datasets.data=[
+    data.AB, data.RB
+  ]},data)
+
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
       <header className="px-5 py-4 border-b border-slate-100 dark:border-slate-700 flex items-center gap-5">
@@ -44,7 +49,6 @@ className="text-black text-xl"
 </div></div>
         <h2 className="font-semibold text-slate-800 dark:text-slate-100">Buildings</h2>
       </header>
-
       <DoughnutChart data={chartData} width={389} height={260} />
     </div>
   );
